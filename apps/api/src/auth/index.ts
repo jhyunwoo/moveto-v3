ffdc0b41d56de4863ca9b8a644cecfcdde256109
jhyunwoo <jhyunwoo0228@gmail.com>
2025-12-29 20:18:23 +0900

    fix sign up error and add sign out

--- apps/api/src/auth/index.ts
+++ apps/api/src/auth/index.ts
@@ -1,10 +1,12 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import signUpApp from "./sign-up";
 import signInApp from "./sign-in";
+import signOutApp from "./sign-out";
 
 const authApp = new OpenAPIHono();
 
 authApp.route("/sign-up", signUpApp);
 authApp.route("/sign-in", signInApp);
+authApp.route("/sign-out", signOutApp);
 
 export default authApp;
